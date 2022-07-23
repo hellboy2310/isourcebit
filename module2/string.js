be5347fb55b1dfe0c50e89080cd6c0ebde842ca6
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-07-24 01:42:51 +0530

    strings done

---
+++ module2/string.js
@@ -0,0 +1,19 @@
+let str = " I am string"
+console.log(str);
+
+let len = str.length;
+console.log(str);
+
+let slicedArr = str.slice(2,8);
+console.log(slicedArr);
+
+
+let firstname = "bhavesh";
+let lastname = "purohit";
+
+let fullname = firstname + lastname;
+console.log(fullname);
+
+
+let splitted = str.split(" ");
+console.log(splitted);
