68786fa004820cbd18fbfe29b18eed29c4027e24
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-07-24 01:25:50 +0530

    function done

---
+++ module2/functions.js
@@ -0,0 +1,26 @@
+function sayHello(){
+    console.log("hello 1");
+}
+
+sayHello();
+
+
+function fn(sum1,sum2)
+{
+    console.log(sum1 + sum2);
+}
+fn(2,3);
+
+function multiply(num1,num2)
+{
+ return num1*num2;
+}
+let ans = multiply(2,3);
+console.log(ans);
+
+
+let a = function sub(num1, num2){
+    return num1 - num2;
+}
+
+console.log(a(10,5));
