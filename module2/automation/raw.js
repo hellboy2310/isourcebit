7216727be979f2971716ab31541f9f60cbc6ee5e
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-07-30 16:04:08 +0530

    google is opened

--- module2/automation/raw.js
+++ module2/automation/raw.js
@@ -1,17 +1,15 @@
-const puppeteer = require('puppeteer');
+const pupeeteer = require('puppeteer');
 
+let broswerPromise = pupeeteer.launch({headless:false});
 
-let browserPromise = puppeteer.launch({headless:false});//promise mila he ki browser open hoga headless false ki wajah se hi browser open hua he
-
-
-
-browserPromise.then(function(browser){
-    console.log("browser is opened");
+broswerPromise.then(function(browser){
     let pagePromise  = browser.newPage();
     return pagePromise;
 }).then(function(page){
-    let urlPromise = page.goto('https://www.google.com/');
+    let urlPromise = page.goto("https://www.google.com/");
+   
     return urlPromise;
+  
 }).then(function(){
-    console.log('google is opened');
+    console.log("google is opened");
 })
